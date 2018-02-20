2ad89cce237397a4cb635dd14499f4aff007b5a0
Roman Fadeev <roma.fadeev@gmail.com>
2018-02-20 21:30:15 +0800

    Initial version of presets browser
    Add presets browser editor window, display all preset assets
    in scrollable list. Draw each list item as ObjectField to allow
    pinging item preset asset.

---
+++ Editor/PresetsBrowserWindow.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEditor;
+using UnityEditor.Presets;
+using UnityEngine;
+
+namespace PresetsBrowser
+{
+    public class PresetsBrowserWindow : EditorWindow
+    {
+        private Vector2 m_scrollPosition;
+
+        [MenuItem("Window/Presets Browser")]
+        private static void Init()
+        {
+            var window = EditorWindow.GetWindow<PresetsBrowserWindow>();
+            window.titleContent = new GUIContent("Presets");
+            window.Show();
+        }
+
+        private void OnGUI()
+        {
+            var presetGuids = AssetDatabase.FindAssets("t:preset");
+
+            m_scrollPosition = EditorGUILayout.BeginScrollView(m_scrollPosition);
+
+            foreach (var presetGuid in presetGuids)
+            {
+                var presetPath = AssetDatabase.GUIDToAssetPath(presetGuid);
+                var preset = AssetDatabase.LoadAssetAtPath<Preset>(presetPath);
+                var presetType = Type.GetType(preset.GetTargetFullTypeName());
+                EditorGUILayout.ObjectField(preset, presetType, false);
+            }
+
+            EditorGUILayout.EndScrollView();
+        }
+    }
+}
