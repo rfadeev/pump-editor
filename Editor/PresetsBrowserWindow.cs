3bedee378c1a56f6aeb602fb34dfb305122500f5
Roman Fadeev <roma.fadeev@gmail.com>
2018-02-24 16:39:39 +0800

    Support filter by preset type
    Add toggle to switch filter mode. Select preset filter via popup
    showing all preset target type full names.

--- Editor/PresetsBrowserWindow.cs
+++ Editor/PresetsBrowserWindow.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEditor.Presets;
 using UnityEngine;
@@ -7,7 +9,12 @@ namespace PresetsBrowser
 {
     public class PresetsBrowserWindow : EditorWindow
     {
+        private List<Preset> m_presets = new List<Preset>();
+        private HashSet<string> m_presetTargetFullTypeNames = new HashSet<string>();
+        private List<Preset> m_presetsToDraw = new List<Preset>();
+        private bool m_filterByPresetType = false;
         private Vector2 m_scrollPosition;
+        private string m_filterPresetType;
 
         [MenuItem("Window/Presets Browser")]
         private static void Init()
@@ -19,18 +26,58 @@ namespace PresetsBrowser
 
         private void OnGUI()
         {
+            m_presets.Clear();
+            m_presetTargetFullTypeNames.Clear();
+            m_presetsToDraw.Clear();
+
             var presetGuids = AssetDatabase.FindAssets("t:preset");
-
-            m_scrollPosition = EditorGUILayout.BeginScrollView(m_scrollPosition);
-
             foreach (var presetGuid in presetGuids)
             {
                 var presetPath = AssetDatabase.GUIDToAssetPath(presetGuid);
                 var preset = AssetDatabase.LoadAssetAtPath<Preset>(presetPath);
-                var presetType = Type.GetType(preset.GetTargetFullTypeName());
-                EditorGUILayout.ObjectField(preset, presetType, false);
+                m_presets.Add(preset);
+
+                var targetFullTypeName = preset.GetTargetFullTypeName();
+                m_presetTargetFullTypeNames.Add(targetFullTypeName);
             }
 
+            m_filterByPresetType = EditorGUILayout.Toggle("Filter by preset type", m_filterByPresetType);
+
+            if (m_filterByPresetType)
+            {
+                var typeNames = m_presetTargetFullTypeNames.ToArray<string>();
+                var index = Array.IndexOf(typeNames, m_filterPresetType);
+                index = EditorGUILayout.Popup(index, typeNames);
+                if (index >= 0 && index < typeNames.Length)
+                {
+                    m_filterPresetType = typeNames[index];
+                    var selectedPresetType = TypeUtility.GetType(m_filterPresetType);
+
+                    foreach (var preset in m_presets)
+                    {
+                        var targetFullTypeName = preset.GetTargetFullTypeName();
+                        var presetType = TypeUtility.GetType(targetFullTypeName);
+                        if (selectedPresetType == presetType)
+                        {
+                            m_presetsToDraw.Add(preset);
+                        }
+                    }
+                }
+                else
+                {
+                    m_filterPresetType = null;
+                }
+            }
+            else
+            {
+                m_presetsToDraw.AddRange(m_presets);
+            }
+
+            m_scrollPosition = EditorGUILayout.BeginScrollView(m_scrollPosition);
+            foreach (var preset in m_presetsToDraw)
+            {
+                EditorGUILayout.ObjectField(preset, null, false);
+            }
             EditorGUILayout.EndScrollView();
         }
     }
---
+++ TypeUtility.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Reflection;
+
+namespace PresetsBrowser
+{
+    public static class TypeUtility
+    {
+        // Solution for getting type by name when running
+        // frm Unity editor is taken from here:
+        // https://answers.unity.com/answers/239152/view.html
+        public static Type GetType(string typeName)
+        {
+            // Try Type.GetType() first. This will work with types defined
+            // by the Mono runtime, in the same assembly as the caller, etc.
+            var type = Type.GetType(typeName);
+
+            // If it worked, then we're done here
+            if (type != null)
+            {
+                return type;
+            }
+
+            // If the TypeName is a full name, then we can try loading the defining assembly directly
+            if (typeName.Contains("."))
+            {
+                // Get the name of the assembly (Assumption is that we are using
+                // fully-qualified type names)
+                var assemblyName = typeName.Substring(0, typeName.IndexOf('.'));
+
+                // Attempt to load the indicated Assembly
+                var assembly = Assembly.Load(assemblyName);
+                if (assembly == null)
+                {
+                    return null;
+                }
+
+                // Ask that assembly to return the proper Type
+                type = assembly.GetType(typeName);
+                if (type != null)
+                {
+                    return type;
+                }
+            }
+
+            // If we still haven't found the proper type, we can enumerate all of the
+            // loaded assemblies and see if any of them define the type
+            var currentAssembly = Assembly.GetExecutingAssembly();
+            var referencedAssemblies = currentAssembly.GetReferencedAssemblies();
+            foreach (var assemblyName in referencedAssemblies)
+            {
+                // Load the referenced assembly
+                var assembly = Assembly.Load(assemblyName);
+                if (assembly != null)
+                {
+                    // See if that assembly defines the named type
+                    type = assembly.GetType(typeName);
+                    if (type != null)
+                        return type;
+                }
+            }
+
+            // The type just couldn't be found...
+            return null;
+        }
+    }
+}
