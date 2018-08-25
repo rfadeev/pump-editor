a7b710f6ddb4a8fa173351e1ad3b64778b31767b
Roman Fadeev <roma.fadeev@gmail.com>
2018-08-25 20:27:32 +0800

    Initial version of project settings select editor window

---
+++ Source/Editor/ProjectSettingsSelect/ProjectSettingsSelectEditorWindow.cs
@@ -0,0 +1,43 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace PumpEditor
+{
+    public class ProjectSettingsSelectEditorWindow : EditorWindow
+    {
+        [MenuItem("Window/Pump Editor/Project Settings Select")]
+        private static void Init()
+        {
+            var window = EditorWindow.GetWindow<ProjectSettingsSelectEditorWindow>();
+            var icon = EditorGUIUtility.Load("SettingsIcon") as Texture2D;
+            window.titleContent = new GUIContent("Project", icon);
+            window.Show();
+        }
+
+        private static void DrawProjectSettingsButton(string menuItemName)
+        {
+            if (GUILayout.Button(menuItemName))
+            {
+                var menuItemPath = "Edit/Project Settings/" + menuItemName;
+                EditorApplication.ExecuteMenuItem(menuItemPath);
+            }
+        }
+
+        private void OnGUI()
+        {
+            DrawProjectSettingsButton("Input");
+            DrawProjectSettingsButton("Tags and Layers");
+            DrawProjectSettingsButton("Audio");
+            DrawProjectSettingsButton("Time");
+            DrawProjectSettingsButton("Player");
+            DrawProjectSettingsButton("Physics");
+            DrawProjectSettingsButton("Physics 2D");
+            DrawProjectSettingsButton("Quality");
+            DrawProjectSettingsButton("Graphics");
+            DrawProjectSettingsButton("Network");
+            DrawProjectSettingsButton("Editor");
+            DrawProjectSettingsButton("Script Execution Order");
+            DrawProjectSettingsButton("Preset Manager");
+        }
+    }
+}
