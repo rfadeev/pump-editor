0bb1a7a0b39835bd3cd3acea0001da81d485abc1
Roman Fadeev <roma.fadeev@gmail.com>
2018-09-20 23:59:22 +0800

    Add initial prefab variant editor window
    Show prefab variants with tree view with
    most derived variants as root nodes.

---
+++ Source/Editor/PrefabVariants/PrefabVariantEditorWindow.cs
@@ -0,0 +1,60 @@
+﻿using UnityEditor;
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+
+namespace PumpEditor
+{
+    // Implementation is based on SimpleTreeViewWindow example
+    // from TreeView manual: https://docs.unity3d.com/Manual/TreeViewAPI.html
+    public class PrefabVariantEditorWindow : EditorWindow
+    {
+        [SerializeField]
+        private TreeViewState treeViewState;
+
+        private PrefabVariantTreeView treeView;
+        private SearchField searchField;
+
+        [MenuItem("Window/Pump Editor/Prefab Variants")]
+        private static void ShowWindow()
+        {
+            var window = GetWindow<PrefabVariantEditorWindow>();
+            window.titleContent = new GUIContent("Prefab Variants");
+            window.Show();
+        }
+
+        private void DoToolbar()
+        {
+            GUILayout.BeginHorizontal(EditorStyles.toolbar);
+            GUILayout.Space(100);
+            GUILayout.FlexibleSpace();
+            treeView.searchString = searchField.OnToolbarGUI(treeView.searchString);
+            GUILayout.EndHorizontal();
+        }
+
+        private void DoTreeView()
+        {
+            var rect = GUILayoutUtility.GetRect(0, 100000, 0, 100000);
+            treeView.OnGUI(rect);
+        }
+
+        private void OnEnable()
+        {
+            // Check if we already had a serialized view state (state
+            // that survived assembly reloading)
+            if (treeViewState == null)
+            {
+                treeViewState = new TreeViewState();
+            }
+
+            treeView = new PrefabVariantTreeView(treeViewState);
+            searchField = new SearchField();
+            searchField.downOrUpArrowKeyPressed += treeView.SetFocusAndEnsureSelectedItem;
+        }
+
+        private void OnGUI()
+        {
+            DoToolbar();
+            DoTreeView();
+        }
+    }
+}
---
+++ Source/Editor/PrefabVariants/PrefabVariantTreeView.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+
+namespace PumpEditor
+{
+    public class PrefabVariantTreeView : TreeView
+    {
+        public PrefabVariantTreeView(TreeViewState treeViewState)
+        : base(treeViewState)
+        {
+            Reload();
+        }
+
+        protected override TreeViewItem BuildRoot()
+        {
+            var root = new TreeViewItem
+            {
+                id = 0,
+                depth = -1
+            };
+
+            var inheritancePaths = GetInheritancePaths();
+            foreach (var inheritancePath in inheritancePaths)
+            {
+                if (inheritancePath.Count == 0)
+                {
+                    continue;
+                }
+
+                var prefabAsset = inheritancePath[0];
+                var prefabItem = new TreeViewItem
+                {
+                    id = prefabAsset.GetInstanceID(),
+                    displayName = prefabAsset.name
+                };
+                root.AddChild(prefabItem);
+
+                var i = 1;
+                while (i < inheritancePath.Count)
+                {
+                    var nestedPrefabAsset = inheritancePath[i];
+                    var nestedPrefabItem = new TreeViewItem
+                    {
+                        id = nestedPrefabAsset.GetInstanceID(),
+                        displayName = nestedPrefabAsset.name
+                    };
+
+                    prefabItem.AddChild(nestedPrefabItem);
+                    prefabItem = nestedPrefabItem;
+
+                    ++i;
+                }
+            }
+
+            SetupDepthsFromParentsAndChildren(root);
+
+            return root;
+        }
+
+        private static List<List<Object>> GetInheritancePaths()
+        {
+            var inheritancePaths = new List<List<Object>>();
+
+            var guids = AssetDatabase.FindAssets("t:Prefab");
+            foreach (var guid in guids)
+            {
+                var assetPath = AssetDatabase.GUIDToAssetPath(guid);
+                var asset = AssetDatabase.LoadAssetAtPath<Object>(assetPath);
+
+                var inheritancePath = new List<Object>();
+                while (PrefabUtility.GetPrefabAssetType(asset) == PrefabAssetType.Variant)
+                {
+                    inheritancePath.Add(asset);
+                    asset = PrefabUtility.GetCorrespondingObjectFromSource(asset);
+                }
+
+                inheritancePath.Add(asset);
+                inheritancePaths.Add(inheritancePath);
+            }
+
+            return inheritancePaths;
+        }
+    }
+}
