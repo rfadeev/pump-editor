6079acebe2e47f3a0bd59603ec0009f18c8b08cb
Roman Fadeev <roma.fadeev@gmail.com>
2020-02-10 22:26:08 +0800

    Add context menu items for force reserialize assets logic
    Extract common serialization methods into utils class.

--- Editor/ForceReserializeAssets/ForceReserializeAssetsEditorWindow.cs
+++ Editor/ForceReserializeAssets/ForceReserializeAssetsEditorWindow.cs
@@ -1,6 +1,5 @@
 ﻿#if UNITY_2017_3_OR_NEWER
 
-using System;
 using UnityEditor;
 using UnityEngine;
 
@@ -17,41 +16,18 @@ namespace PumpEditor
             window.Show();
         }
 
-        private static void ForceReserializeAllAssets()
-        {
-            if (!EditorUtility.DisplayDialog("Attention", "Do you want to force reserialize all assets? This can be time heavy operation and result in massive list of changes.", "Ok", "Cancel"))
-            {
-                return;
-            }
-
-            AssetDatabase.ForceReserializeAssets();
-        }
-
-        private static void ForceReserializeSelectedAssets()
-        {
-            var assetGUIDs = Selection.assetGUIDs;
-            if (assetGUIDs.Length == 0)
-            {
-                EditorUtility.DisplayDialog("Attention", "No assets are selected.", "Ok");
-                return;
-            }
-
-            var assetPaths = Array.ConvertAll<string, string>(assetGUIDs, AssetDatabase.GUIDToAssetPath);
-            AssetDatabase.ForceReserializeAssets(assetPaths);
-        }
-
         private void OnGUI()
         {
             using (new EditorGUILayout.VerticalScope(GUI.skin.box))
             {
                 if (GUILayout.Button("Force Reserialize All Assets"))
                 {
-                    ForceReserializeAllAssets();
+                    ForceReserializeAssetsUtils.ForceReserializeAllAssets();
                 }
 
                 if (GUILayout.Button("Force Reserialize Selected Assets"))
                 {
-                    ForceReserializeSelectedAssets();
+                    ForceReserializeAssetsUtils.ForceReserializeSelectedAssets();
                 }
             }
         }
