0b83e94a7c60353e33cc0202811bbe4ef54c42d6
Roman Fadeev <roma.fadeev@gmail.com>
2020-03-04 23:22:01 +0800

    Add save project as template editor window barebones

---
+++ Editor/ProjectTemplates/SaveProjectAsTemplateEditorWindow.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace PumpEditor
+{
+    public class SaveProjectAsTemplateEditorWindow : EditorWindow
+    {
+        private static readonly string UnityEditorApplicationProjectTemplatesPath = Path.Combine(
+            Path.GetDirectoryName(EditorApplication.applicationPath),
+            "Data",
+            "Resources",
+            "PackageManager",
+            "ProjectTemplates"
+        );
+
+        private string targetPath;
+        private string templateName;
+        private string templateDisplayName;
+        private string templateDescription;
+        private string templateDefaultScene;
+        private string templateVersion;
+
+        [MenuItem("Window/Pump Editor/Save Project As Template")]
+        private static void ShowWindow()
+        {
+            var window = EditorWindow.GetWindow<SaveProjectAsTemplateEditorWindow>();
+            window.titleContent = new GUIContent("Save Project As Template");
+            window.Show();
+        }
+
+        private void InvokeSaveProjectAsTemplate()
+        {
+            Assembly editorAssembly = Assembly.GetAssembly(typeof(Editor));
+            Type editorUtilityType = editorAssembly.GetType("UnityEditor.EditorUtility");
+
+            MethodInfo methodInfo = editorUtilityType.GetMethod("SaveProjectAsTemplate", BindingFlags.Static | BindingFlags.NonPublic);
+            methodInfo.Invoke(editorUtilityType, new object[]{ targetPath, templateName, templateDisplayName, templateDescription, templateDefaultScene, templateVersion});
+        }
+
+        private void OnGUI()
+        {
+            if (GUILayout.Button("Select Target Folder"))
+            {
+                targetPath = EditorUtility.SaveFolderPanel("Choose target folder", UnityEditorApplicationProjectTemplatesPath, "");
+            }
+
+            EditorGUILayout.HelpBox("Path is readonly text field, to input value click \"Select Target Folder\" button.", MessageType.Info);
+            EditorGUILayout.TextField("Path:", targetPath);
+
+            templateName = EditorGUILayout.TextField("Name:", templateName);
+            templateDisplayName = EditorGUILayout.TextField("Display name:", templateDisplayName);
+            templateDescription = EditorGUILayout.TextField("Description:", templateDescription);
+            templateDefaultScene = EditorGUILayout.TextField("Default scene:", templateDefaultScene);
+            templateVersion = EditorGUILayout.TextField("Version:", templateVersion);
+
+            if (GUILayout.Button("Save"))
+            {
+                AssetDatabase.SaveAssets();
+                InvokeSaveProjectAsTemplate();
+            }
+        }
+    }
+}
