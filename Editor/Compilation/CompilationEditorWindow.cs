00ea6f7f3e8e35249a953ae2e18c107f066fd318
Roman Fadeev <roma.fadeev@gmail.com>
2020-06-01 11:58:21 +0800

    Add initial compilation editor window

---
+++ Editor/Compilation/CompilationEditorWindow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEditor.Compilation;
+using UnityEngine;
+
+namespace PumpEditor
+{
+    public class CompilationWindow : EditorWindow
+    {
+        [MenuItem("Window/Pump Editor/Compilation")]
+        private static void ShowWindow()
+        {
+            var window = EditorWindow.GetWindow<CompilationWindow>();
+            window.titleContent = new GUIContent("Compilation");
+            window.Show();
+        }
+
+        private void OnGUI()
+        {
+            if (GUILayout.Button("Request Script Compilation"))
+            {
+                CompilationPipeline.RequestScriptCompilation();
+            }
+        }
+    }
+}
