a0a3a7fdb0fcc70825f0aa3f1642c536c2537461
Roman Fadeev <roma.fadeev@gmail.com>
2020-11-26 20:46:35 +0100

    Initial log files window
    Show editor log path for Windows and allow to print it to console.

---
+++ Editor/LogFiles/LogFilePathsAPI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PumpEditor
+{
+    // File paths based on https://docs.unity3d.com/2019.4/Documentation/Manual/LogFiles.html
+    public static class LogFilePathsAPI
+    {
+        private const string EDITOR_LOG_PATH = "C:\\Users\\{0}\\AppData\\Local\\Unity\\Editor\\Editor.log";
+
+        public static string GetEditorLogPath()
+        {
+            return String.Format(EDITOR_LOG_PATH, Environment.UserName);
+        }
+    }
+}
---
+++ Editor/LogFiles/LogFilesEditorWindow.cs
@@ -0,0 +1,40 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace PumpEditor
+{
+    public class LogFilesEditorWindow : EditorWindow
+    {
+        [MenuItem("Window/Pump Editor/Log Files")]
+        private static void ShowWindow()
+        {
+            var window = EditorWindow.GetWindow<LogFilesEditorWindow>();
+            window.titleContent = new GUIContent("Log Files");
+            window.Show();
+        }
+
+        private static void DrawLogFileItem(string name, string logFilePath)
+        {
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                EditorGUILayout.TextField(name, logFilePath);
+
+                if (GUILayout.Button("Log path to console"))
+                {
+                    Debug.Log(logFilePath);
+                }
+            }
+        }
+
+        private static void EditorLogsGUI()
+        {
+            EditorGUILayout.LabelField("Editor", EditorStyles.boldLabel);
+            DrawLogFileItem("Editor log", LogFilePathsAPI.GetEditorLogPath());
+        }
+
+        private void OnGUI()
+        {
+            EditorLogsGUI();
+        }
+    }
+}
