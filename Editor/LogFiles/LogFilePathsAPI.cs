5d83f783a54acf3799c147276c97d3bc444273c7
Roman Fadeev <roma.fadeev@gmail.com>
2020-11-28 12:33:44 +0100

    Support macOS and Linux log file paths

--- Editor/LogFiles/LogFilePathsAPI.cs
+++ Editor/LogFiles/LogFilePathsAPI.cs
@@ -7,10 +7,25 @@ namespace PumpEditor
     // File paths based on https://docs.unity3d.com/2019.4/Documentation/Manual/LogFiles.html
     public static class LogFilePathsAPI
     {
+        private const string MACOS_PACKAGE_MANAGER_LOG_PATH = "~/Library/Logs/Unity/upm.log";
+        private const string MACOS_EDITOR_LOG_PATH = "~/Library/Logs/Unity/Editor.log";
+        private const string MACOS_EDITOR_LOG_PREV_PATH = "~/Library/Logs/Unity/Editor-prev.log";
+        private const string MACOS_PLAYER_LOG_PATH = "~/Library/Logs/{0}/{1}/Player.log";
+
+        private const string LINUX_PACKAGE_MANAGER_LOG_PATH = "~/.config/unity3d/upm.log";
+        private const string LINUX_EDITOR_LOG_PATH = "~/.config/unity3d/Editor.log";
+        private const string LINUX_EDITOR_LOG_PREV_PATH = "~/.config/unity3d/Editor-prev.log";
+        private const string LINUX_PLAYER_LOG_PATH = "~/.config/unity3d/{0}/{1}/Player.log";
+
         private static readonly string WINDOWS_PACKAGE_MANAGER_LOG_PATH;
         private static readonly string WINDOWS_EDITOR_LOG_PATH;
         private static readonly string WINDOWS_EDITOR_LOG_PREV_PATH;
         private static readonly string WINDOWS_PLAYER_LOG_PATH;
+
+        private static readonly string PACKAGE_MANAGER_LOG_PATH;
+        private static readonly string EDITOR_LOG_PATH;
+        private static readonly string EDITOR_LOG_PREV_PATH;
+        private static readonly string PLAYER_LOG_PATH;
 
         static LogFilePathsAPI()
         {
@@ -22,26 +37,50 @@ namespace PumpEditor
             // While Unity docs state file name as Player.log, it seems to be an error in the docs.
             var appDataLocalLowPath = Path.GetFullPath(Path.Combine(appDataLocalPath, "..", "LocalLow"));
             WINDOWS_PLAYER_LOG_PATH = Path.Combine(appDataLocalLowPath, "{0}", "{1}", "output_log.txt");
+
+            switch (Application.platform)
+            {
+                case RuntimePlatform.WindowsEditor:
+                    PACKAGE_MANAGER_LOG_PATH = WINDOWS_PACKAGE_MANAGER_LOG_PATH;
+                    EDITOR_LOG_PATH = WINDOWS_EDITOR_LOG_PATH;
+                    EDITOR_LOG_PREV_PATH = WINDOWS_EDITOR_LOG_PREV_PATH;
+                    PLAYER_LOG_PATH = WINDOWS_PLAYER_LOG_PATH;
+                    break;
+                case RuntimePlatform.OSXEditor:
+                    PACKAGE_MANAGER_LOG_PATH = MACOS_PACKAGE_MANAGER_LOG_PATH;
+                    EDITOR_LOG_PATH = MACOS_EDITOR_LOG_PATH;
+                    EDITOR_LOG_PREV_PATH = MACOS_EDITOR_LOG_PREV_PATH;
+                    PLAYER_LOG_PATH = MACOS_PLAYER_LOG_PATH;
+                    break;
+                case RuntimePlatform.LinuxEditor:
+                    PACKAGE_MANAGER_LOG_PATH = LINUX_PACKAGE_MANAGER_LOG_PATH;
+                    EDITOR_LOG_PATH = LINUX_EDITOR_LOG_PATH;
+                    EDITOR_LOG_PREV_PATH = LINUX_EDITOR_LOG_PREV_PATH;
+                    PLAYER_LOG_PATH = LINUX_PLAYER_LOG_PATH;
+                    break;
+                default:
+                    break;
+            }
         }
 
         public static string GetPackageManagerLogPath()
         {
-            return WINDOWS_PACKAGE_MANAGER_LOG_PATH;
+            return PACKAGE_MANAGER_LOG_PATH;
         }
 
         public static string GetEditorLogPath()
         {
-            return WINDOWS_EDITOR_LOG_PATH;
+            return EDITOR_LOG_PATH;
         }
 
         public static string GetEditorLogPrevPath()
         {
-            return WINDOWS_EDITOR_LOG_PREV_PATH;
+            return EDITOR_LOG_PREV_PATH;
         }
 
         public static string GetPlayerLogPath()
         {
-            return String.Format(WINDOWS_PLAYER_LOG_PATH, Application.companyName, Application.productName);
+            return String.Format(PLAYER_LOG_PATH, Application.companyName, Application.productName);
         }
     }
 }
