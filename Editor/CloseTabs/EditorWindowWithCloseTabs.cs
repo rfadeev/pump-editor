8e92367e42c50a819868b5aef72318e9fe8ab00c
Roman Fadeev <roma.fadeev@gmail.com>
2021-04-25 23:41:24 +0200

    Add editor window with close tabs abstract class
    To simplify support of close tabs in new windows.

---
+++ Editor/CloseTabs/EditorWindowWithCloseTabs.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+namespace PumpEditor
+{
+    public abstract class EditorWindowWithCloseTabs : EditorWindow, IHasCustomMenu
+    {
+        public virtual void AddItemsToMenu(GenericMenu menu)
+        {
+            CloseTabsHelper.AddCloseOtherTabsItem(this, menu);
+        }
+    }
+}
